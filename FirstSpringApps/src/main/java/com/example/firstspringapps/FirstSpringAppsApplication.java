d7cca9ed7efc1128fdea80c716d82f16215284a2
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-04-20 21:28:22 +0530

    added: Spring App to Desplay Massege

---
+++ FirstSpringApps/src/main/java/com/example/firstspringapps/FirstSpringAppsApplication.java
@@ -0,0 +1,12 @@
+package com.example.firstspringapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppsApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello From Bridge Labz");
+    }
+}
